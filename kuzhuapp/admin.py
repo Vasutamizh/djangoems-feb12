527a2b7b631f7de3ad08fcdef7c5666a7a77e559
vasutamizh <vdev8594@gmail.com>
2024-03-31 11:00:42 +0530

    feat: add admin panel

--- kuzhuapp/admin.py
+++ kuzhuapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Member,Emi
 # Register your models here.
+
+
+class Memberadmin(admin.ModelAdmin):
+    fields=['Name','Age','Gaurdian','Aadhaarnumber','Pannumber','Loanamount','Image']
+
+
+class Emiadmin(admin.ModelAdmin):
+    fields=['mem','repay','Intrest','Savings','Sandha']
+
+
+admin.site.register(Member, Memberadmin)
+admin.site.register(Emi, Emiadmin)
